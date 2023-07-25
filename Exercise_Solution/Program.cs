3b976ab4ccd8f4ebdf88377aa4f3465c4d13778f
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-07-25 14:00:42 +0300

    Created a method for user withing to input his or her own example to check the solution

--- Exercise_Solution/Program.cs
+++ Exercise_Solution/Program.cs
@@ -11,3 +11,31 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+// Метод введения пользовательского примера для проверки решения
+string[] InputYourExampleArray ()
+{
+    Console.Write("Input the number of elements in your array: ");
+    int arraySize = Convert.ToInt32(Console.ReadLine());
+    while (arraySize <= 0)
+    {
+        Console.Write("Try again with your number > 0: ");
+        arraySize = Convert.ToInt32(Console.ReadLine());
+    }
+
+    string [] userArray = new string[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        Console.WriteLine($"Input your word №{i+1}:");
+        userArray[i] = Console.ReadLine();
+        while (userArray[i] == "")
+        {
+            Console.WriteLine("Try again. You must type something:");
+            userArray[i] = Console.ReadLine();
+        }
+    }
+    return userArray;
+}
+
